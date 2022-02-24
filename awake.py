bf1e9b22290c26cb0654b5308fdc356e99c9d5e6
KadenFranklin <43596236+KadenFranklin@users.noreply.github.com>
2022-02-24 16:56:08 -0600

    it kept messing up i hope it doesnt now

--- awake.py
+++ awake.py
@@ -2,27 +2,27 @@ import time
 import pyautogui
 
 
-def check_if_move():
-    x, y = pyautogui.position()
-    time.sleep(2)
-    xx, yy = pyautogui.position()
-    if x != xx | y != yy:
-        check_if_move()
-    else:
-        return None
-    
+def check_if_move(x, y):
+	time.sleep(2)
+	xx, yy = pyautogui.position()
+	if x == xx:
+		return None
+	if y == yy:
+		return None
+	else:
+		check_if_move(xx, yy)
+
 
 while True:
-    check_if_move()
-    x1, y1 = pyautogui.position()
-    pyautogui.move(500, 0, duration=.3, tween=pyautogui.easeInOutQuad)
-    x2, y2 = pyautogui.position()
-    pyautogui.moveTo(x2 + 50, y2, duration=0.01, tween=pyautogui.easeInOutQuad)
-    pyautogui.moveTo(x2 + 75, y2 + 43, duration=0.01, tween=pyautogui.easeInOutQuad)
-    pyautogui.moveTo(x2 + 50, y2 + 86, duration=0.01, tween=pyautogui.easeInOutQuad)
-    pyautogui.moveTo(x2, y2 + 86, duration=0.01, tween=pyautogui.easeInOutQuad)
-    pyautogui.moveTo(x2 - 25, y2 + 43, duration=0.01, tween=pyautogui.easeInOutQuad)
-    pyautogui.moveTo(x2, y2, duration=0.01, tween=pyautogui.easeInOutQuad)
-    pyautogui.moveTo(x1, y1, duration=.1, tween=pyautogui.easeInOutQuad)
-    time.sleep(10)
-    
+	x1, y1 = pyautogui.position()
+	check_if_move(x1, y1)
+	pyautogui.move(500, 0, duration=.3, tween=pyautogui.easeInOutQuad)
+	x2, y2 = pyautogui.position()
+	pyautogui.moveTo(x2 + 50, y2, duration=0.01, tween=pyautogui.easeInOutQuad)
+	pyautogui.moveTo(x2 + 75, y2 + 43, duration=0.01, tween=pyautogui.easeInOutQuad)
+	pyautogui.moveTo(x2 + 50, y2 + 86, duration=0.01, tween=pyautogui.easeInOutQuad)
+	pyautogui.moveTo(x2, y2 + 86, duration=0.01, tween=pyautogui.easeInOutQuad)
+	pyautogui.moveTo(x2 - 25, y2 + 43, duration=0.01, tween=pyautogui.easeInOutQuad)
+	pyautogui.moveTo(x2, y2, duration=0.01, tween=pyautogui.easeInOutQuad)
+	pyautogui.moveTo(x1, y1, duration=.1, tween=pyautogui.easeInOutQuad)
+	time.sleep(10)
